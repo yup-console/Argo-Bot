070d4359e78b4ed72ca2de85542915af765ac3f0
Console <138242046+yup-console@users.noreply.github.com>
2025-07-07 08:27:50 +0530

    Add files via upload

---
+++ src/config/emoji.js
@@ -0,0 +1,11 @@
+module.exports = {
+    author: "<:author:1279037469739847722>",
+    requester: "<:requestor:1279037472449368136>",
+    duration: "<:duration:1279037475016409128>",
+    tick: "<:sec_tick:1286524512489443409>",
+    cross: "<:Crossmark:1286524517417750613>",
+    playing: "<a:Playing:1279047119256420362>",
+    warn: "<:Warning:1289441679002308722>",
+    enable: "<:Enabled:1249975128637444129>",
+    disable: "<:disabled:1249975213819428936>",
+  };
