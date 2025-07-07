798d91a65a3a4c64c7369c3d6cc9a46d3f3a7814
Console <138242046+yup-console@users.noreply.github.com>
2025-07-07 08:29:23 +0530

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  token:
+    process.env.token ||
+    "TOKEN_HERE",
+  prefix: ",",
+  color: "#ff0000",
+  Mongo: "mongodb+srv://techargodev:uIaAwmIDqWpJk6f6@sumant.118tf.mongodb.net/?retryWrites=true&w=majority&appName=Sumant",
+  ownerIDS: ["958583892326117437","1033232690222223380"],
+  vote: true,
+  image: "https://media.discordapp.net/attachments/1241172642799816766/1249187753875210291/Picsart_24-04-17_17-00-34-515.jpg?ex=66666441&is=666512c1&hm=6f4268997b45cb645285eaf1fa5e62aa496d3fceb72969dd3092ae06f8d7f292&=&format=webp&width=1191&height=670",
+  setupBgLink: "https://media.discordapp.net/attachments/1241172642799816766/1249187753875210291/Picsart_24-04-17_17-00-34-515.jpg?ex=66d27f41&is=66d12dc1&hm=da10e01c44aed1732ee9a1b428698a67d308199d2322841ead68e7acec13bbe7&=&format=webp&width=1066&height=599",
+  invite:
+    "https://discordapp.com/oauth2/authorize?client_id=1131938711639183363",
+  inviteTwo: "https://discordapp.com/oauth2/authorize?client_id=939758802532716575&permissions=45768987437137&scope=bot%20applications.commands",
+  inviteThree: "https://discordapp.com/oauth2/authorize?client_id=939758802532716575&permissions=45768987437137&scope=bot%20applications.commands",  
+  ssLink: "https://discord.gg/PGE6mxURus",
+  topGg: "https://top.gg/bot/1131938711639183363",
+  topgg_Api: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjExMzE5Mzg3MTE2MzkxODMzNjMiLCJib3QiOnRydWUsImlhdCI6MTcxMjcwOTg1M30.WaNFIy3-UhJ95WxfgmPC2smwMp-9hfTwCQ4_WJZ7Ji0",
+  noprefixLogWebhook: "https://discord.com/api/webhooks/1286534928905011200/qHD5tS88isuLhwO-ghlDB-UD0pL90gSBca7tTK3bl-Guss8UeN9LtLyBk04UIzaa_mgI",
+  cmd_log: "https://discord.com/api/webhooks/1305150940336099388/TmOSKgREaJR2-tqM1aqLa-CO6WPtVOd21unmws5ueGuug9zG-0vfKYJzSBgGtK5JCuCc",
+  error_log: "https://discord.com/api/webhooks/1305151070904778752/BRZF5KPBb2izHQTSdKc__YJac_a-JC0dt1myrnfy4oc8dnHFCKXd2UtwRnVzndRK3I61",
+  blacklist_log: "https://discord.com/api/webhooks/1305151236135321600/vU30jzMFx-I49T1YKAT9bDkJ62i7v_RA4dGUHtp67cbmCkTr3iK1rIxAvKs0nWgY4Urj",
+  join_log: "https://discord.com/api/webhooks/1305151481753636925/U3ttWGo7euOORlYcQ_1t0fmUuaJJaYr2569l3eCVz0SuJcUFftLUGOBTcToY-NMNoFTK",
+  leave_log: "https://discord.com/api/webhooks/1305151794867077203/V3Pvr9e054zMv9yJGMhM8GTTiLWMb3ImeA4FBhX2kBzuibAmj-7iEU_bLuAHyZZcYjku",
+  spotiId: "e6f84fbec2b44a77bf35a20c5ffa54b8",
+  spotiSecret: "498f461b962443cfaf9539c610e2ea81",
+  nodes: [
+    {
+      // name: "Argo",
+      // url: "v4.lavalink.rocks:443",
+      // auth: "horizxon.tech",
+      // secure: true,
+      name: "Argo",
+      url: "45.89.99.118:8000",
+      auth: "winkle@team",
+      secure: false,
+    },
+  ],
+};
